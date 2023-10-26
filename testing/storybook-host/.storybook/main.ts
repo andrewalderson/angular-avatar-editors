aa8d123d80df1288101e47a01d6e56cfcf4f71ea
Andrew Alderson <andrew.alderson@outlook.com>
2023-10-26 15:06:41 +0000

    test: add and configure storybook

---
+++ testing/storybook-host/.storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../../../packages/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  staticDirs: ['../assets'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+};
+
+export default config;
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/recipes/storybook/custom-builder-configs
