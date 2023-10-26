0fdd4c2944556c612896b79136a1953f3ea47141
Andrew Alderson <andrew.alderson@outlook.com>
2023-10-26 16:19:01 +0000

    feat(basic-avatar-editor): add initial image

---
+++ packages/basic-avatar-editor/src/lib/avatar-editor-image.directive.ts
@@ -0,0 +1,7 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: 'img[ngxAvatarEditorImage]',
+  standalone: true,
+})
+export class NgxAvatarEditorImageDirective {}
--- packages/basic-avatar-editor/src/lib/avatar-editor.component.stories.ts
+++ packages/basic-avatar-editor/src/lib/avatar-editor.component.stories.ts
@@ -1,13 +1,32 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+import { NgxAvatarEditorImageDirective } from './avatar-editor-image.directive';
 import { NgxAvatarEditorComponent } from './avatar-editor.component';
 
 const meta: Meta<NgxAvatarEditorComponent> = {
   component: NgxAvatarEditorComponent,
   title: 'Components/BasicAvatarEditor',
+  decorators: [
+    moduleMetadata({
+      imports: [NgxAvatarEditorImageDirective],
+    }),
+  ],
+  parameters: {
+    layout: 'centered',
+  },
 };
 export default meta;
-type Story = StoryObj<NgxAvatarEditorComponent>;
+type Story = StoryObj<NgxAvatarEditorComponent & HTMLImageElement>;
+
+const Template: Story = {
+  render: (args) => ({
+    props: args,
+    template: `<ngx-avatar-editor><img ngxAvatarEditorImage [src]="src"/></ngx-avatar-editor>`,
+  }),
+};
 
 export const Primary: Story = {
-  args: {},
+  ...Template,
+  args: {
+    src: 'grace-hilty-uvAhJHLul_Q-unsplash.jpg',
+  },
 };
