be26b399d7de89813438f352915227f5de001681
Andrew Alderson <andrew.alderson@outlook.com>
2023-10-26 15:57:04 +0000

    chore: add initial microsoft avatar editor

---
+++ packages/microsoft-avatar-editor/src/lib/avatar-editor.component.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { NgxAvatarEditorComponent } from './avatar-editor.component';
+
+const meta: Meta<NgxAvatarEditorComponent> = {
+  component: NgxAvatarEditorComponent,
+  title: 'Components/MicrosoftAvatarEditor',
+};
+export default meta;
+type Story = StoryObj<NgxAvatarEditorComponent>;
+
+export const Primary: Story = {
+  args: {},
+};
